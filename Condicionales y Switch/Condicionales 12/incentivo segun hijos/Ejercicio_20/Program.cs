c3b37daf1eddcbd3beb5f646eda1f33ac6b80a02
Amly De los Santos :} <161159877+amlyderos@users.noreply.github.com>
2025-03-06 07:32:09 +1300

    Add files via upload

---
+++ Condicionales y Switch/Condicionales 12/incentivo segun hijos/Ejercicio_20/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ingrese el sueldo del empleado:");
+        double sueldo = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Ingrese el número de hijos del empleado:");
+        int numeroHijos = Convert.ToInt32(Console.ReadLine());
+
+        double incentivo = 0;
+        if (numeroHijos > 0)
+        {
+            incentivo = numeroHijos * 500;
+        }
+
+        Console.WriteLine("Sueldo: " + sueldo);
+        Console.WriteLine("Número de hijos: " + numeroHijos);
+
+        if (incentivo > 0)
+        {
+            Console.WriteLine("Incentivo: " + incentivo);
+            Console.WriteLine("Sueldo total: " + (sueldo + incentivo));
+        }
+        else
+        {
+            Console.WriteLine("Incentivo: N/A");
+        }
+    }
+}
