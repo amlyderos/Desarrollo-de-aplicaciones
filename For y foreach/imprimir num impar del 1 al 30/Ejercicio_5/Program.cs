6909db3cb73756e47c447d110fe476d74fa55166
Amly De los Santos :} <161159877+amlyderos@users.noreply.github.com>
2025-03-06 08:05:25 +1300

    Add files via upload

---
+++ For y foreach/imprimir num impar del 1 al 30/Ejercicio_5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+
+
+        for (int i = 1; i <= 30; i++)
+        {
+
+           if ( i % 2 != 0)
+            {
+                Console.WriteLine(i);
+            }
+        }
+      
+
+
+        
+
+
+
+    }
+}
