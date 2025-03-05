a2c30c0e36db7a3fa687913b594a29868698deea
Amly De los Santos :} <161159877+amlyderos@users.noreply.github.com>
2025-03-06 08:07:21 +1300

    Add files via upload

---
+++ For y foreach/tabla de mutiplicar del 1 al 12/Ejercicio_5/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+
+        double resultado;
+        double num;
+        
+
+        Console.Write("Ingrese un número: ");
+        num = Convert.ToDouble(Console.ReadLine());
+
+
+        for (int i = 1; i <= 12; i++)
+        {
+
+
+            resultado = num * i;
+
+            Console.WriteLine($"{num} * {i} = {resultado}");
+
+
+        }
+
+
+
+
+
+
+
+
+    }
+}
