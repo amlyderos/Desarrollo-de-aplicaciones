696b6ac3b316357db74ced8931273dae719dc961
Amly De los Santos :} <161159877+amlyderos@users.noreply.github.com>
2025-03-06 10:26:25 +1300

    Add files via upload

---
+++ Programacion pro Capas/Prueba Tecnica de Biblioteca/CapaEntidades/Entidades.cs
@@ -0,0 +1,31 @@
+﻿namespace CapaEntidades
+{
+    public class Entidades
+    {
+        
+        public int ID_Prestamo { get; set; }
+
+        public string NombreUsuario { get; set; }
+
+        public string CorreoCliente { get; set; }
+
+        public string Fecha_Prestamo { get; set; }
+
+        public string Fecha_Devolucion { get; set; }
+
+        public int Estado { get; set; }
+
+
+
+        public int Id_Libro { get; set; }
+
+        public string Titulo { get; set; }
+
+        public string Autor { get; set; }
+
+        public string Categoria { get; set; }
+
+        public string CantidadDisponible { get; set; }
+        
+    }
+}
