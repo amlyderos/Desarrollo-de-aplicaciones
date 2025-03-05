bf12ebc8027640f0fb0200d5395a77fd753c964b
Amly De los Santos :} <161159877+amlyderos@users.noreply.github.com>
2025-03-06 08:10:11 +1300

    Add files via upload

---
+++ For y foreach/patrón triangular con asteriscos/Ejercicio_30/Ejercicio_30/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace PatrónTriangular
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int filas;
+
+            Console.Write("Ingrese el número de filas para el patrón triangular: ");
+            filas = Convert.ToInt32(Console.ReadLine());
+
+            
+            for (int i = 1; i <= filas; i++)
+            {
+
+
+                
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+
+
+
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
