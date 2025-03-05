d994fd92767275322f0511a100825596a631c518
Amly De los Santos :} <161159877+amlyderos@users.noreply.github.com>
2025-03-06 08:00:32 +1300

    Add files via upload

---
+++ For y foreach/Sumatoria de numeros enteros menor a 100/Ejercicio_24/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Ingrese la cantidad de números a sumar: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        int suma = 0;
+
+
+
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write("Ingrese el número " + (i + 1) );
+            int numero = Convert.ToInt32(Console.ReadLine());
+            suma = suma + numero; 
+        }
+
+       
+
+
+        if (suma >= 100)
+        {
+            Console.WriteLine("La sumatoria es mayor o igual a 100.");
+            
+        }
+
+
+
+        else
+        {
+            Console.WriteLine("La sumatoria es menor a 100.");
+        }
+
+    }
+}
