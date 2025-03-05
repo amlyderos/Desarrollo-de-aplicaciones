f3007165b1b365af8da56f45c3e221d8db087b39
Amly De los Santos :} <161159877+amlyderos@users.noreply.github.com>
2025-03-06 07:23:37 +1300

    Add files via upload

---
+++ Condicionales y Switch/Condicionales 4/literal de estudiantes/Ejercicio_5/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        double calificación;
+
+
+        Console.Write("Ingrese la calificación del estudiante: ");
+        calificación = Convert.ToDouble(Console.ReadLine());
+
+
+        if (calificación > 100)
+        {
+            Console.WriteLine("La calificación no puede ser mayor a 100:");
+        }
+
+        else if (calificación >= 90)
+        {
+            Console.WriteLine("La calificación ingresada fue: " + calificación);
+
+            Console.WriteLine("¡La situación del estudiante esta excelente!");
+        }
+
+        else if (calificación >= 80)
+        {
+            Console.WriteLine("La calificación ingresada fue: " + calificación);
+
+            Console.WriteLine("¡La situación del estudiante esta muy bien!");
+        }
+
+        else if (calificación >= 75)
+        {
+            Console.WriteLine("La calificación ingresada fue: " + calificación);
+
+            Console.WriteLine("¡La situación del estudiante esta bien!");
+        }
+
+        else if (calificación >= 70)
+        {
+            Console.WriteLine("La calificación ingresada fue: " + calificación);
+
+            Console.WriteLine("¡La situación del estudiante esta regular!");
+        }
+
+        else 
+        {
+            Console.WriteLine("La calificación ingresada fue: " + calificación);
+
+            Console.WriteLine("¡La situación del estudiante esta deficiente!");
+        }
+
+        
+
+
+        
+
+
+
+    }
+}
