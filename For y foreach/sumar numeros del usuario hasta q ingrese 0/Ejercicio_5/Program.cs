b6fe6d356c8f234300cfee1ad272e866b89c5a3d
Amly De los Santos :} <161159877+amlyderos@users.noreply.github.com>
2025-03-06 08:09:07 +1300

    Add files via upload

---
+++ For y foreach/sumar numeros del usuario hasta q ingrese 0/Ejercicio_5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        int num;
+        
+        int sumatoria = 0;
+
+        
+
+            while (true) 
+        {
+            Console.WriteLine("Ingrese un número" );
+            num = Convert.ToInt32( Console.ReadLine() );
+
+            if ( num == 0 ) 
+            {
+                break;
+
+            }
+
+            sumatoria += num;
+        }
+
+        Console.WriteLine("La sumatoria de los números ingresados es: " + sumatoria);
+
+    }
+}
