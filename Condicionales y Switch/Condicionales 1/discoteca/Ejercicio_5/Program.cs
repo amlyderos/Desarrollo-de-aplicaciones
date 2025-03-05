8efac9dede2effb18edea28e4bab7dce8de41426
Amly De los Santos :} <161159877+amlyderos@users.noreply.github.com>
2025-03-06 07:20:33 +1300

    Add files via upload

---
+++ Condicionales y Switch/Condicionales 1/discoteca/Ejercicio_5/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        double edad ;
+        string genero;
+       
+
+
+
+        Console.Write("Ingrese su edad: ");
+        edad = Convert.ToInt32(Console.ReadLine());
+
+        if (edad >= 18)
+        {
+            Console.Write("Ingrese su genero: ");
+            genero = Convert.ToString(Console.ReadLine());
+
+
+            if (genero == "Hembra" || genero == "hembra")
+            {
+                Console.Write("Puedes pasar gratis ");
+            }
+
+            else
+            {
+                Console.Write("Debes pagar para pasar");
+            }
+        }
+
+        else
+        {
+            Console.Write("No puedes pasar");
+        }
+
+
+    }
+}
