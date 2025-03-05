83a806e76470d44f0a857beb33f859d0e7df20de
Amly De los Santos :} <161159877+amlyderos@users.noreply.github.com>
2025-03-06 10:26:57 +1300

    Add files via upload

---
+++ Programacion pro Capas/Prueba Tecnica de Biblioteca/CapaNegocio/Logica.cs
@@ -0,0 +1,42 @@
+﻿using System.Data;
+using CapaDatos;
+using CapaEntidades;
+
+namespace CapaNegocio
+{
+    public class Logica
+    {
+        public static DataTable Listar_prestamo()
+        {
+            Prestamos datos = new Prestamos();
+            DataTable resultado = datos.Listar_Prestamo();
+            return resultado;
+        }
+
+
+        public static string Guardar_prestamo(Entidades datos)
+        {
+            Prestamos data = new Prestamos();
+            return data.Guardar_prestamo(datos);
+        }
+
+        public static DataTable Buscar_prestamo(string dato)
+        {
+            Prestamos datos = new Prestamos();
+            DataTable resultado = datos.Buscar_prestamo(dato);
+            return resultado;
+        }
+
+        public static string Eliminar_prestamo(Entidades id)
+        {
+            Prestamos num = new Prestamos();
+            return num.Eliminar_prestamo(id);
+        }
+
+        public static string Actualizar_prestamo(Entidades datos)
+        {
+            Prestamos actualizarDatos = new Prestamos();
+            return actualizarDatos.Actualizar_prestamo(datos);
+        }
+    }
+}
---
+++ Programacion pro Capas/Prueba Tecnica de Biblioteca/CapaNegocio/Logica2.cs
@@ -0,0 +1,54 @@
+﻿using CapaDatos;
+using CapaEntidades;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaNegocio
+{
+    public class Logica2
+    {
+        public static DataTable Listar_libro()
+        {
+            Libros datos = new Libros();
+            DataTable resultado = datos.Listar_Libro();
+            return resultado;
+        }
+
+        public static DataTable ListarCategoria()
+        {
+            Libros datos = new Libros();
+            DataTable resultado = datos.ListarCategoria();
+            return resultado;
+        }
+
+
+        public static string Guardar_libro(Entidades datos)
+        {
+            Libros data = new Libros();
+            return data.Guardar_libro(datos);
+        }
+
+        public static DataTable Buscar_Libro(string dato)
+        {
+            Libros datos = new Libros();
+            DataTable resultado = datos.Buscar_Libro(dato);
+            return resultado;
+        }
+
+        public static string Eliminar_Libro(Entidades id)
+        {
+            Libros num = new Libros();
+            return num.Eliminar_Libro(id);
+        }
+
+        public static string Actualizar_Libro(Entidades datos)
+        {
+            Libros actualizarDatos = new Libros();
+            return actualizarDatos.Actualizar_Libro(datos);
+        }
+    }
+}
