4f412d91eb97e264d220727fce6c3f86d5fdb3d5
Amly De los Santos :} <161159877+amlyderos@users.noreply.github.com>
2025-03-06 07:51:23 +1300

    Add files via upload

---
+++ For y foreach/contraseña correcta o no/Ejercicio_5/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+
+        int contraseña_definida = 1234;
+        int contraseña_ingresada; 
+
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write("Ingrese la contraseña: ");
+                contraseña_ingresada = Convert.ToInt32(Console.ReadLine());
+
+                if (contraseña_ingresada == contraseña_definida)
+                {
+                    Console.WriteLine("La contraseña es correcta");
+                    return;
+                }
+
+                else
+                {
+                    Console.WriteLine("La contraseña es incorrecta");
+                  
+                    
+                }
+
+            
+        }
+
+
+        Console.WriteLine("Ha agotado el número de intentos");
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
