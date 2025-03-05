fdf183b8d38ab01a8cf326d252bcbb612aacb428
Amly De los Santos :} <161159877+amlyderos@users.noreply.github.com>
2025-03-06 08:03:39 +1300

    Add files via upload

---
+++ For y foreach/promedio de numeros naturales/Ejercicio_26/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 100;
+            double suma = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+               suma = suma + i;
+            }
+
+            double promedio = suma / n;
+
+            Console.WriteLine("El promedio de los primeros " + n + " numeros naturales es: " + promedio);
+
+            Console.ReadKey();
+        }
+    }
