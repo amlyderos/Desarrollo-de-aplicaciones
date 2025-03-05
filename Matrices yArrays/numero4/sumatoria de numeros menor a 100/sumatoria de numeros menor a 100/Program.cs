567ec972293b810f271b3103c6ff9be661e9da3c
Amly De los Santos :} <161159877+amlyderos@users.noreply.github.com>
2025-03-06 08:20:03 +1300

    Add files via upload

---
+++ Matrices yArrays/numero4/sumatoria de numeros menor a 100/sumatoria de numeros menor a 100/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicios_de_Gamalier_matrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Cuantos numeros quieres ingresar: ");
+            int[] posiciones = new int[Convert.ToInt32(Console.ReadLine())];
+            int[] numeros1 = new int[posiciones.Length];
+
+            for (int i = 0; i < posiciones.Length; i++)
+            {
+                Console.Write("Ingresa el numero: ");
+                numeros1[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("");
+            Console.Write(numeros1.Sum());
+
+
+            if (numeros1.Sum() >= 100)
+            {
+                Console.Write("Es mayor o igual a 100");
+            }
+            else
+            {
+                Console.Write("No es mayor o igual a 100");
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Matrices yArrays/numero4/sumatoria de numeros menor a 100/sumatoria de numeros menor a 100/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("sumatoria de numeros menor a 100")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("sumatoria de numeros menor a 100")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("518f0378-8412-4bc3-a909-93edd1314d72")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
