9baa927776ceb69649bb01128b7ef55254bfa66a
Amly De los Santos :} <161159877+amlyderos@users.noreply.github.com>
2025-03-06 08:01:50 +1300

    Add files via upload

---
+++ For y foreach/suma de numeros naturales/Ejercicio_25/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int suma = 0;
+            for (int i = 1; i <= 100; i++)
+            {
+                suma = suma + i;
+            }
+            Console.WriteLine("La suma de los primeros 100 números naturales es: " + suma);
+            Console.ReadKey();
+        }
+    }
